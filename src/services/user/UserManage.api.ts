169c0646fa5c5d11b2fb1d0ee075311d12e5f08b
EiG123 <narinsuwan567@gmail.com>
2026-05-26 16:27:49 +0700

    fix api

--- src/services/user/UserManage.api.ts
+++ src/services/user/UserManage.api.ts
@@ -3,7 +3,7 @@ import { api } from "../api.api";
 export const UserManage = {
     async getProfileData(data: any) {
         const response = await api.post(
-            "/getProfileData", data
+            "/UserLocation/getProfileData", data
         );
         return response.data.data;
     },
@@ -11,7 +11,7 @@ export const UserManage = {
     async updateProfile(data: any) {
         console.log(data);
         const response = await api.post(
-            "/updateProfile", data
+            "/UserManage/updateProfile", data
         );
         return response.data.data;
     }
